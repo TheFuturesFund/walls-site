a133694332f277805df760826657dc1b4025668d
Jonathan Hooper <jon9820@gmail.com>
2018-04-28 14:42:06 -0500

    resize and pad sponsor marquee

--- _plugins/sponsor_marquee.rb
+++ _plugins/sponsor_marquee.rb
@@ -5,7 +5,10 @@ Jekyll::Hooks.register :site, :after_init do
   image_list = Magick::ImageList.new
   images_filenames.each do |filename|
     image = read_and_resize_image(filename)
-    image_list.push(image) unless image.nil?
+    break if image.nil?
+    image_list.push(image)
+    padding_image = Magick::Image.new(30, 100) { self.background_color = 'white' }
+    image_list.push(padding_image)
   end
   output_image = image_list.append(false)
   output_image.alpha Magick::BackgroundAlphaChannel
@@ -14,7 +17,7 @@ end
 
 def read_and_resize_image(filename)
   image = Magick::Image.read(filename).first
-  image.resize_to_fit(9999, 200)
+  image.resize_to_fit(9999, 100)
 rescue Magick::ImageMagickError => e
   warn "Error reading sponsor '#{filename}' image for marquee: #{e}"
 end
