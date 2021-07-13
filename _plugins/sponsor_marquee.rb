6ed791ff571e4519b38005c7fd459b28e3c5d89b
Damien Vesper <ldamienvesper@gmail.com>
2021-07-13 14:00:44 -0500

    sponsor_marquee: lint

--- _plugins/sponsor_marquee.rb
+++ _plugins/sponsor_marquee.rb
@@ -1,24 +1,24 @@
 require 'rmagick'
 
 Jekyll::Hooks.register :site, :after_init do
-  images_filenames = Dir['img/home/sponsors/*']
-  image_list = Magick::ImageList.new
-  images_filenames.each do |filename|
-    image = read_and_resize_image(filename)
-    break if image.nil?
-    image_list.push(image)
-    padding_image = Magick::Image.new(30, 100) { self.background_color = 'white' }
-    image_list.push(padding_image)
-  end
-  output_image = image_list.append(false)
-  output_image.background_color = 'white'
-  output_image.alpha Magick::BackgroundAlphaChannel
-  output_image.write('img/home/sponsor_marquee.jpg')
+    images_filenames = Dir['img/home/sponsors/*']
+    image_list = Magick::ImageList.new
+    images_filenames.each do |filename|
+        image = read_and_resize_image(filename)
+        break if image.nil?
+        image_list.push(image)
+        padding_image = Magick::Image.new(30, 100) { self.background_color = 'white' }
+        image_list.push(padding_image)
+    end
+    output_image = image_list.append(false)
+    output_image.background_color = 'white'
+    output_image.alpha Magick::BackgroundAlphaChannel
+    output_image.write('img/home/sponsor_marquee.jpg')
 end
 
 def read_and_resize_image(filename)
-  image = Magick::Image.read(filename).first
-  image.resize_to_fit(9999, 100)
+    image = Magick::Image.read(filename).first
+    image.resize_to_fit(9999, 100)
 rescue Magick::ImageMagickError => e
-  warn "Error reading sponsor '#{filename}' image for marquee: #{e}"
+    warn "Error reading sponsor '#{filename}' image for marquee: #{e}"
 end
